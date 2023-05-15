b6e0f03d69379a131041a98f1672446550608c4e
Sergio De Luca <serg.r.deluca@gmail.com>
2023-05-15 17:45:04 -0300

    intento template con bootstrap

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+
+export const AdminLayoutRoutes: Routes = [
+    // { path: 'login',          component: LoginComponent },
+    // { path: 'dashboard',      component: DashboardComponent },
+    // { path: 'user',           component: UserComponent },
+    // { path: 'table',          component: TableComponent },
+    // { path: 'typography',     component: TypographyComponent },
+    // { path: 'icons',          component: IconsComponent },
+    // { path: 'maps',           component: MapsComponent },
+    // { path: 'notifications',  component: NotificationsComponent },
+    // { path: 'upgrade',        component: UpgradeComponent }
+];
