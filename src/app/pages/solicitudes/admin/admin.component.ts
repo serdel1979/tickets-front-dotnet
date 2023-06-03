ac700a9e1cf8988db909fe1332183f5d68cfdeff
Sergio De Luca <serg.r.deluca@gmail.com>
2023-06-03 19:03:17 -0300

    probando signalr

--- src/app/pages/solicitudes/admin/admin.component.ts
+++ src/app/pages/solicitudes/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { SolicitudesService } from '../../../services/solicitudes.service';
 import { AuthService } from '../../../auth/auth.service';
 import { Solicitud } from 'src/app/interfaces/solicitud.interface';
 import { Router } from '@angular/router';
-import { HubConnectionBuilder } from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from 'src/environments/environment';
 import { url } from 'inspector';
 
@@ -19,47 +19,36 @@ export class AdminComponent implements OnInit {
 
   public isLogued: boolean = false;
   public page!: number;
-
+  private connection: HubConnection;
   public solicitudes!: Solicitud[];
 
   constructor(private solicitudesService: SolicitudesService,
     private authService: AuthService,
-    private router: Router) { }
-
-  async ngOnInit(): Promise<void> {
-
-    console.log(URLHub);
-    const connection = new HubConnectionBuilder()
+    private router: Router) {
+    this.connection = new HubConnectionBuilder()
       .withUrl(URLHub) // URL del concentrador en tu servidor
       .build();
 
-     // Inicia la conexión con el servidor
-     connection.start().then(() => {
+    // Inicia la conexión con el servidor
+    
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.connection.start().then(() => {
       // La conexión se ha establecido correctamente
       console.log("conexión socket ok...");
+      this.connection.on("Refresh", () => {
+        this.refresh(); // Llamar al método refresh para actualizar los datos
+    });
     }).catch(err => {
       console.error(err.toString());
     });
-
-    connection.on('Refresh', async (userId: string) => {
-      // Lógica para recargar los datos en el cliente para el usuario específico
-      console.log('Recibida notificación de recarga de datos para el usuario:', userId);
-      // Aquí puedes llamar a la función o método para recargar los datos en tu componente
-      await this.refresh();
-    });
-
-   
-    // this.isLogued = this.authService.isLogued();
-    // if (this.isLogued) {
-    //   this.solicitudesService.getSolicitudes().subscribe(resp => {
-    //     this.solicitudes = resp;
-    //   })
-    // }
     await this.refresh()
   }
 
 
-  async refresh(){
+  async refresh() {
+    console.log("haciendo refresh");
     this.isLogued = this.authService.isLogued();
     if (this.isLogued) {
       this.solicitudesService.getSolicitudes().subscribe(resp => {
