a2aabc41149c9356732f3350dd45d28c266760e0
Sergio De Luca <serg.r.deluca@gmail.com>
2023-07-24 16:22:15 -0300

    eliminados los console.log

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,48 +9,54 @@ import { AuthService } from '../../auth/auth.service';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
   private listTitles: any[] = [];
   location: Location;
   private nativeElement: Node;
   private toggleButton!: Element;
   private sidebarVisible: boolean;
 
+  public logued: boolean = false;
+
   public isCollapsed = true;
-  @ViewChild("navbar-cmp", {static: false}) button: any;
+  @ViewChild("navbar-cmp", { static: false }) button: any;
 
-  constructor(private authService: AuthService, location:Location, private renderer : Renderer2, private element : ElementRef, private router: Router) {
-      this.location = location;
-      this.nativeElement = element.nativeElement;
-      this.sidebarVisible = false;
+  constructor(private authService: AuthService, location: Location, private renderer: Renderer2, private element: ElementRef, private router: Router) {
+    this.location = location;
+    this.nativeElement = element.nativeElement;
+    this.sidebarVisible = false;
   }
 
-  ngOnInit(){
-      this.listTitles = ROUTES.filter((listTitle: any) => listTitle);
-      var navbar : HTMLElement = this.element.nativeElement;
-      this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
-      this.router.events.subscribe((event) => {
-        this.sidebarClose();
-     });
+  ngOnInit() {
+    this.logued = this.authService.isLogued();
+    this.authService.isLoggedInChange.subscribe((isLogued)=>{
+      this.logued = isLogued;
+    })
+    this.listTitles = ROUTES.filter((listTitle: any) => listTitle);
+    var navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
+    this.router.events.subscribe((event) => {
+      this.sidebarClose();
+    });
   }
-  getTitle(){
+  getTitle() {
     var titlee = this.location.prepareExternalUrl(this.location.path());
-    if(titlee.charAt(0) === '#'){
-        titlee = titlee.slice( 1 );
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
     }
-    for(var item = 0; item < this.listTitles.length; item++){
-        if(this.listTitles[item].path === titlee){
-            return this.listTitles[item].title;
-        }
+    for (var item = 0; item < this.listTitles.length; item++) {
+      if (this.listTitles[item].path === titlee) {
+        return this.listTitles[item].title;
+      }
     }
     titlee = titlee.slice(1);
     const stringOriginal = titlee;
     const stringsSeparados = stringOriginal.split("/");
     var titulo = '';
-    if(stringsSeparados.length > 0){
-      if(stringsSeparados[1]){
+    if (stringsSeparados.length > 0) {
+      if (stringsSeparados[1]) {
         titulo = `${stringsSeparados[0]} ${stringsSeparados[1]}`;
-      }else{
+      } else {
         titulo = `${stringsSeparados[0]}`;
       }
       return titulo;
@@ -59,54 +65,54 @@ export class NavbarComponent implements OnInit{
   }
 
   sidebarToggle() {
-      if (this.sidebarVisible === false) {
-          this.sidebarOpen();
-      } else {
-          this.sidebarClose();
-      }
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
     }
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const html = document.getElementsByTagName('html')[0];
-        const mainPanel =  <HTMLElement>document.getElementsByClassName('main-panel')[0];
-        setTimeout(function(){
-            toggleButton.classList.add('toggled');
-        }, 500);
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    const mainPanel = <HTMLElement>document.getElementsByClassName('main-panel')[0];
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
 
-        html.classList.add('nav-open');
-        if (window.innerWidth < 991) {
-          mainPanel.style.position = 'fixed';
-        }
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-        const html = document.getElementsByTagName('html')[0];
-        const mainPanel =  <HTMLElement>document.getElementsByClassName('main-panel')[0];
-        if (window.innerWidth < 991) {
-          setTimeout(function(){
-            mainPanel.style.position = '';
-          }, 500);
-        }
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        html.classList.remove('nav-open');
-    };
-    collapse(){
-      this.isCollapsed = !this.isCollapsed;
-      const navbar = document.getElementsByTagName('nav')[0];
-      if (!this.isCollapsed) {
-        navbar.classList.remove('navbar-transparent');
-        navbar.classList.add('bg-white');
-      }else{
-        navbar.classList.add('navbar-transparent');
-        navbar.classList.remove('bg-white');
-      }
-
+    html.classList.add('nav-open');
+    if (window.innerWidth < 991) {
+      mainPanel.style.position = 'fixed';
+    }
+    this.sidebarVisible = true;
+  };
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    const mainPanel = <HTMLElement>document.getElementsByClassName('main-panel')[0];
+    if (window.innerWidth < 991) {
+      setTimeout(function () {
+        mainPanel.style.position = '';
+      }, 500);
+    }
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  };
+  collapse() {
+    this.isCollapsed = !this.isCollapsed;
+    const navbar = document.getElementsByTagName('nav')[0];
+    if (!this.isCollapsed) {
+      navbar.classList.remove('navbar-transparent');
+      navbar.classList.add('bg-white');
+    } else {
+      navbar.classList.add('navbar-transparent');
+      navbar.classList.remove('bg-white');
     }
 
-    salir(){
-      this.authService.logout();
-      this.router.navigateByUrl('/login');
-    }
+  }
+
+  salir() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 
 }
