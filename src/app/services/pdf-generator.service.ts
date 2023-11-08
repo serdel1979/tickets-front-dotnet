891222a3f388addeb874a522cae45dbffb1ff441
sergio <sdlbsso@gmail.com>
2023-11-08 10:28:50 -0300

    configuracion de reportes historial

--- src/app/services/pdf-generator.service.ts
+++ src/app/services/pdf-generator.service.ts
@@ -8,68 +8,65 @@ export class PdfGeneratorService {
 
   constructor() { }
 //`${new Date().toISOString()}_tutorial.pdf`
-  generatePdfgeneral(data: any[]) {
+
+
+  generatePdfgeneral(data: any[]){
     const pdf = new PdfMakeWrapper();
 
-    pdf.info({
-      title: `Reporte ${new Date().toISOString()}`
-    });
-
+    pdf.pageOrientation('landscape'); // Configura la orientación de la página
+    
     pdf.add(
       new Txt(`Historial al ${new Date().toLocaleDateString('es-AR')}`)
         .fontSize(13)
         .bold()
-        .margin([0, 0, 0, 0])
         .end
     );
-
+    
     const formattedData = data.map(item => ({
       ...item,
       fecha: new Date(item.fecha).toLocaleDateString('es-AR')
     }));
-
+    
     pdf.add(
       new Table([
         ['Depto', 'Fecha', 'Equipo', 'Descripción', 'Estado'],
         ...formattedData.map(item => [item.departamento, item.fecha, item.equipo, item.descripcion, item.estadoActual])
       ])
-        .layout('lightHorizontalLines') // Optional: Add horizontal lines to separate the rows
+        .layout('lightHorizontalLines')
         .end
     );
-
+    
     pdf.create().open();
+    
   }
+
 
   generatePdf(data: any[],user: string) {
     const pdf = new PdfMakeWrapper();
-    user = user.toUpperCase();
 
-    pdf.info({
-      title: `Reporte ${new Date().toISOString()}`
-    });
-
+    pdf.pageOrientation('landscape'); // Configura la orientación de la página
+    
     pdf.add(
       new Txt(`Historial al ${new Date().toLocaleDateString('es-AR')} perteneciente a ${user}`)
         .fontSize(13)
         .bold()
-        .margin([0, 0, 0, 0])
         .end
     );
-
+    
     const formattedData = data.map(item => ({
       ...item,
       fecha: new Date(item.fecha).toLocaleDateString('es-AR')
     }));
-
+    
     pdf.add(
       new Table([
-        ['Fecha', 'Equipo', 'Descripción', 'Estado'],
-        ...formattedData.map(item => [item.fecha, item.equipo, item.descripcion, item.estadoActual])
+        ['Depto', 'Fecha', 'Equipo', 'Descripción', 'Estado'],
+        ...formattedData.map(item => [item.departamento, item.fecha, item.equipo, item.descripcion, item.estadoActual])
       ])
-        .layout('lightHorizontalLines') // Optional: Add horizontal lines to separate the rows
+        .layout('lightHorizontalLines')
         .end
     );
-
+    
     pdf.create().open();
   }
 
